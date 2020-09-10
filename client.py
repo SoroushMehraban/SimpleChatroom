64fa50c94979b76782007ba58cc41d50152d7761
Soroush Mehraban <Smehraban2013@gmail.com>
2020-09-10 12:07:04 +0430

    document added to program

--- client.py
+++ client.py
@@ -10,6 +10,7 @@ TCP_port = 0
 MESSAGE_LENGTH = 1024 * 1024 * 2  # 2MB is maximum length
 
 
+# gets server host name from the first argument of the program. if there isn't then gets it manually
 def get_server_host_name():
     global SERVER_HOST_NAME
     try:
@@ -19,6 +20,8 @@ def get_server_host_name():
         SERVER_HOST_NAME = input("> ")
 
 
+# gets server port number from the second argument of the program. if there isn't any or its invalid then gets it
+# manually
 def get_server_port_number():
     global SERVER_PORT_NUMBER
     try:
@@ -33,6 +36,9 @@ def get_server_port_number():
                 print("Your input should be an integer")
 
 
+# gets client name from the third argument of program. if there isn't then gets it manually
+# after getting client name. sends a request to server to make sure it's unique. if it's not then tells user to pick
+# another one
 def get_client_name():
     global CLIENT_NAME
     try:
@@ -48,6 +54,9 @@ def get_client_name():
         connected_successfully = check_client_name()
 
 
+# this function connects to the server and sends it a checkuser message (explained in server.py)
+# it make sures that client name is unique
+# if its unique it returns 0 otherwise it returns 1
 def check_client_name():
     client = make_connection(SERVER_HOST_NAME, SERVER_PORT_NUMBER)
     send_tcp_message(client, "checkuser:{} {}:{}".format(CLIENT_NAME, 'localhost', TCP_port))
@@ -61,6 +70,7 @@ def check_client_name():
     client.close()
 
 
+# this function tries to make a connection to the server. if server rejected it exits the program
 def make_connection(address, port):
     try:
         client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -68,14 +78,17 @@ def make_connection(address, port):
         return client
     except Exception:
         print("Connection failed")
-        exit(1)
+        os._exit(1)
 
 
+# this function decode the message into a binary format and sends it via socket like a stream
 def send_tcp_message(source_socket, msg):
     message = msg.encode("utf-8")
     source_socket.send(message)
 
 
+# this is a client server side function which just receives messages from others in the group
+# for better understanding you have to look start_tcp_server in server.py
 def start_tcp_server():
     global TCP_port
 
@@ -89,6 +102,7 @@ def start_tcp_server():
         threading.Thread(target=handle_received_message, args=[connection]).start()
 
 
+# this function handles every message given by server by just printing it
 def handle_received_message(connection):
     message = connection.recv(MESSAGE_LENGTH).decode("utf-8")
     print(message)
@@ -97,6 +111,7 @@ def handle_received_message(connection):
     sys.stdout.flush()
 
 
+# shows guidance to the user at the first
 def show_guidance():
     print("Guidance:")
     print("join GROUP_ID  -> Join to group with GROUP_ID in server, if there isn't creates one")
@@ -106,6 +121,10 @@ def show_guidance():
     print("----------------------------------------------------------------------------------")
 
 
+# this function is called when user tries to join a group in chatroom
+# it does his job whenever the given format is "join [group_id]" so the length is 2
+# it makes a connection to server, sends a tcp message to it and waits for response
+# after receiving response, it prints it and then closes the connection
 def send_join_request(given_command):
     if len(given_command.split(" ")) == 2:
         client = make_connection(SERVER_HOST_NAME, SERVER_PORT_NUMBER)
@@ -116,6 +135,10 @@ def send_join_request(given_command):
         client.close()
 
 
+# this function is called when user tries to send a message to a group
+# it does his job whenever the given format is "send [group id] [message]" so the length is more than 2
+# if makes a connection to server, sends the message to it and waits for response
+# after receiving response it prints it then closes the connection
 def send_message_request(given_command):
     if len(given_command.split(" ")) >= 3:
         client = make_connection(SERVER_HOST_NAME, SERVER_PORT_NUMBER)
@@ -129,6 +152,10 @@ def send_message_request(given_command):
         client.close()
 
 
+# this function is called when user tries to leave a group in chatroom
+# it does his job whenever the given format is "leave [group id]" so the length is equal to 2
+# if makes a connection to server, sends the message to it and waits for response
+# after receiving response it prints it then closes the connection
 def send_leave_request(given_command):
     if len(given_command.split(" ")) == 2:
         client = make_connection(SERVER_HOST_NAME, SERVER_PORT_NUMBER)
@@ -138,19 +165,16 @@ def send_leave_request(given_command):
         print(response)
 
 
+# this function is called whenever user types quit and wants to quit the program
+# it makes a connection to server and let him know that by sending "QUIT: [client name]"
+# so by doing this server will know and delete users record so someone else can login with this client name again
 def send_quit_request():
     client = make_connection(SERVER_HOST_NAME, SERVER_PORT_NUMBER)
     send_tcp_message(client, "QUIT: {}".format(CLIENT_NAME))
 
 
-if __name__ == "__main__":
-    threading.Thread(target=start_tcp_server).start()
-    get_server_host_name()
-    get_server_port_number()
-    get_client_name()
-
-    show_guidance()
-
+# this function gets command from user. so it's basically the program user interface
+def get_commands():
     while True:
         command = input("> ")
         if command.startswith("join"):
@@ -161,4 +185,14 @@ if __name__ == "__main__":
             send_leave_request(command)
         if command == 'quit':
             send_quit_request()
-            os._exit(0)
+            os._exit(0)  # exiting the entire threads
+
+
+if __name__ == "__main__":
+    threading.Thread(target=start_tcp_server).start()
+    get_server_host_name()
+    get_server_port_number()
+    get_client_name()
+
+    show_guidance()
+    get_commands()
--- server.py
+++ server.py
@@ -4,8 +4,8 @@ import sys
 
 SERVER_ADDRESS = 'localhost'
 try:
-    SERVER_PORT = int(sys.argv[1])
-except (ValueError, IndexError):
+    SERVER_PORT = int(sys.argv[1])  # getting server port from first argument
+except (ValueError, IndexError):  # if argument is empty or port is not an integer:
     while True:
         try:
             print("Enter server port number:")
@@ -15,10 +15,14 @@ except (ValueError, IndexError):
             print("Your input should be an integer")
 
 MESSAGE_LENGTH = 1024 * 1024 * 2  # 2MB is maximum length
-current_users_dict = {}
-group_dict = {}
+current_users_dict = {}  # holds users which are currently connected to this server as a key and their host info as a value
+group_dict = {}  # holds group id as a key and list of users inside a group as a value
 
 
+# This function starts our TCP server
+# at first it creates a socket. socket.AF_INET means  to user IPv4 and socket.SOCK_STREAM means to use TCP as a transport layer
+# then it binds to our host info which is localhost:[given_port_by_user]
+# after creating socket it listens and after receiving a request it gives it to another function in separate thread
 def start_tcp_server():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((SERVER_ADDRESS, SERVER_PORT))
@@ -33,9 +37,14 @@ def start_tcp_server():
         threading.Thread(target=handle_request, args=[connection]).start()
 
 
+# function to handle requests given to server
+# it first decode received binary message as a utf-8 and then do its job based on given message
 def handle_request(connection):
     message = connection.recv(MESSAGE_LENGTH).decode("utf-8")
     print("Message Received: {}".format(message))
+
+    # if someone try to login client gives its name and we have to decide if his name is unique or not
+    # the format of the message is "checkuser:[client name] [client host info]"
     if message.startswith("checkuser:"):
         user = message.split("checkuser:")[1].split(" ")[0]
         user_host_info = message.split(" ")[1]
@@ -45,8 +54,14 @@ def handle_request(connection):
         else:
             current_users_dict[user] = user_host_info
             connection.send("OK NAME".encode("utf-8"))
+            print("Current users:")
             print(current_users_dict)
+            print("-------------------")
 
+    # if someone tries to join a group chat client sends a join request
+    # the format of the message is "join [group id] [client name]"
+    # if there isn't any group with a group id given after join request then we create one
+    # if we have we join that user into that group
     if message.startswith("join"):
         group_id = message.split(" ")[1]
         user = message.split(" ")[2]
@@ -60,6 +75,9 @@ def handle_request(connection):
             group_dict[group_id] = [user]
             connection.send("[SERVER MESSAGE] Joined successfully".encode("utf-8"))
 
+    # if someone tries to send a message into a group client sends a MESSAGE requests
+    # the format of the message is "MESSAGE: [client name] [group id] [message to send]"
+    # we check if user is in group or not. if he is we send the message to the group otherwise we show him an error
     if message.startswith("MESSAGE:"):
         client_name = message.split(" ")[1]
         group_id = message.split(" ")[2]
@@ -72,6 +90,10 @@ def handle_request(connection):
             connection.send("[SERVER MESSAGE] Message sent successfully".encode("utf-8"))
         else:
             connection.send("[SERVER MESSAGE] You're not joined in this group. please join first".encode("utf-8"))
+
+    # if someone tries to leave a group client sends a LEAVE request
+    # the format of the message is "LEAVE: [group id] [client name]"
+    # if he is in group we remove him and let him know that otherwise we show him an error
     if message.startswith("LEAVE:"):
         group_id = message.split(" ")[1]
         user = message.split(" ")[2]
@@ -83,6 +105,10 @@ def handle_request(connection):
                 connection.send("[SERVER MESSAGE] You're not in this group".encode("utf-8"))
         else:
             connection.send("[SERVER MESSAGE] This group doesn't exist".encode("utf-8"))
+
+    # when user wants to quit a program. before quiting client sends a QUIT request
+    # the format of the message is "QUIT: [client name]"
+    # we remove the user on both dictionary so that someone can login with his client name after that
     if message.startswith("QUIT:"):
         client_name = message.split(" ")[1]
         del current_users_dict[client_name]
@@ -91,6 +117,7 @@ def handle_request(connection):
                 group_dict[group].remove(client_name)
 
 
+# this function sends @message_to_send to everyone in group with @group_id except @sender
 def send_message_to_group(message_to_send, group_id, sender):
     for user in group_dict[group_id]:
         if user == sender:
@@ -102,6 +129,7 @@ def send_message_to_group(message_to_send, group_id, sender):
         user_socket.close()
 
 
+# this function tries to make a connection to a user with @user_info which is a string like "IP_address:port"
 def make_connection(user_info):
     user_ip = user_info.split(":")[0]
     user_port = int(user_info.split(":")[1])
@@ -110,10 +138,10 @@ def make_connection(user_info):
         user_socket.connect((user_ip, user_port))
         return user_socket
     except Exception:
-        print("Connection failed")
-        exit(1)
+        print("Connection failed to user {}".format(user_info))
 
 
+# this function encode the string in binary format and sends the message via given socket
 def send_tcp_message(source_socket, msg):
     message = msg.encode("utf-8")
     source_socket.send(message)
